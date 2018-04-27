a7f8f0de065fa42c4a16377a1ee9b2ab6a4ea0dd
Masen Matthews <masenmatthews@gmail.com>
2018-04-27 10:46:30 -0700

    add initial routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root :to => 'layouts#index'
+
+  resources :posts do
+    resources :comments
+  end
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'sessions#create'
+  get '/signout' => 'sessions#destroy'
 end

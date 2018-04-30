b1db583d2f755a2bcae9ed0c3f02d2dc6f0d79ad
Masen Matthews <masenmatthews@gmail.com>
2018-04-29 17:46:20 -0700

    add validations, fix some tests - 5/7 tests passing

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  validates :comment_body, :presence => true
 
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
+  validates :post_title, :post_body, :presence => true
+
 end

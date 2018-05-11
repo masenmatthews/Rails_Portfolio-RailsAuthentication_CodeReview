ec2b29d13beb05e202907660c27bd1a96ba6a58c
Masen Matthews <masenmatthews@gmail.com>
2018-05-11 13:15:27 -0700

    fix image orientation issue

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/pictures/:style/missing.png"
+  has_attached_file :image, styles: { original: "", medium: "300x300>", thumb: "100x100>" } , source_file_options: { all: '-auto-orient' } , default_url: "/pictures/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   validates :post_title, :post_body, :presence => true

e369869a31ca3d29a9d658525f4057dfdee0877d
Masen Matthews <masenmatthews@gmail.com>
2018-05-11 11:29:29 -0700

    troubleshoot styling/display on blog detail post page

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,6 @@ class Post < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/pictures/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-
   validates :post_title, :post_body, :presence => true
 
 end
